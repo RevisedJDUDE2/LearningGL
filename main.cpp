2f96ae072818ccb669b9e9b8f243cceec6d3bca9
JDUDE official2 <JDUDEofficial134@outlook.ph>
2024-04-20 16:17:04 +0800

    finaly!

--- main.cpp
+++ main.cpp
@@ -8,27 +8,6 @@
 #include "Shader.h"
 
 int main() {
-
-  const char* VertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
-    "out vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
-
-  const char* FragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(ourColor, 1.0f);\n"
-    "}\0";
-
   Shader refVertex;
   std::string vshSource = refVertex.ReadShaderFile("shader.vert");
   const char* VertexshdrSource = vshSource.c_str();
