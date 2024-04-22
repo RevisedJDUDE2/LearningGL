4e19e2dbf6372874ce85f8e96b4eb4681ddbf820
JDUDE official2 <JDUDEofficial134@outlook.ph>
2024-04-22 16:52:36 +0800

    hmmmm.... why?

--- Buffer.c
+++ Buffer.c
@@ -1,9 +1,15 @@
 #include "Buffer.h"
+#include <stdio.h>
 
 void Buffer_CreateBuffer(Buffer_t* buff) {
+  if (buff->count == NULL) {
+    printf("Here\n");
+    glGenBuffers(1, &buff->handle);
+  }
   glGenBuffers(buff->count, &buff->handle);
   glBindBuffer(buff->target, buff->handle);
   glBufferData(buff->target, buff->size, buff->data, buff->usage);
+  printf("Count = %u\n", buff->count);
 }
 
 void Buffer_Unbind(Buffer_t* buff) {
--- main.c
+++ main.c
@@ -24,19 +24,27 @@ int main() {
      0.0f, 0.5f, 0.0f,
      0.5f, -0.5f, 0.0f
   };
+  GLuint VAO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
 
   Buffer_t VBO;
-  VBO.count = 1;
   VBO.target = GL_VERTEX_ARRAY;
   VBO.size = sizeof(Vertices);
   VBO.data = &Vertices;
   VBO.usage = GL_STATIC_DRAW;
   Buffer_CreateBuffer(&VBO);
 
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(Vertices), (void*)0);
+  glEnableVertexAttribArray(0);
+
   Buffer_Unbind(&VBO);
+
+  glBindVertexArray(0);
 
   while (!glfwWindowShouldClose(Window)) {
     glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glfwSwapBuffers(Window);
     glfwPollEvents();
